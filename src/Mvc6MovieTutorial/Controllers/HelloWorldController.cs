38ba4e655c503d8e65608c22bc15ec9d780c1403
Bryan Whyte <bwhyte21@outlook.com>
2016-05-07 17:07:56 -0400

    First Commit!
    The initial commit for the MVC6 Tutorial I'm learning! Yay!

---
+++ src/Mvc6MovieTutorial/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+namespace Mvc6MovieTutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public string Index(){ return "This is my default action!"; }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome() { return "This is the Welcome action method, so WELCOME!!"; }
+
+
+    }
+}
